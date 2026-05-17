f28c3c3c5b76d398b70921f9f3ad650aefb4d23e
Matheus <matheusfdeoliveira2006@gmail.com>
2026-05-17 01:33:05 -0300

    aceitando somente números no input amount

---
+++ script.js
@@ -0,0 +1,8 @@
+// Seleciona os elementos do formulário
+const amount = document.getElementById("amount")
+
+amount.oninput = () => {
+    let value = amount.value.replace(/\D/g, "")
+    
+    amount.value = value
+}
