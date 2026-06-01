c471be624d7c93b9f7cd391935e9cc71c89e4a71
Matheus <matheusfdeoliveira2006@gmail.com>
2026-06-01 16:11:15 -0300

    exibindo a quantidade de itens da lista

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const category = document.getElementById("category")
 
 // Seleciona os elementos da lista.
 const expenseList = document.querySelector("ul")
+const expensesQuantity = document.querySelector ("aside header p span")
 
 // Captura o evento de input para formatar o valor
 amount.oninput = () => {
@@ -49,6 +50,7 @@ form.onsubmit = (event) => {
     expenseAdd(newExpense)
 }
 
+// Adiciona um novo item na lista.
 function expenseAdd(newExpense){
     try {
         // Cria o elemento para adicionar o item (li) na lista (ul).
@@ -94,9 +96,25 @@ function expenseAdd(newExpense){
         // Adiciona o item na lista.
         expenseList.append(expenseItem)
 
+        // Atualiza os totais
+        updateTotals()
+
     } catch (error) {
         alert("Não foi possivel atualizar a lista de despesas.")
         console.log(error)
     }
+}
+
+ // Atualiza os totais
+ function updateTotals() {
+ try {
+    // Recupera todos os itens (li) da lista (ul)
+    const items = expenseList.children
     
+    //Atualiza a quantidade de itens na lista
+    expensesQuantity.textContent = `${items.length} ${items.length > 1 ? "despesas" : "despesa"}`
+ }  catch (error) {
+    console.log(error)
+    alert("Não foi possível atualizar os totais.")
+ }
 }
